f1bb069b9490e75cc419f8299ccebe20ee5f4f07
SHIKIDA LAB <shikidalab_mac@lc-ap2.jim.kochi-tech.ac.jp>
2021-04-01 15:51:53 +0900

    比較元になるファイルをハッシュ値で命名するように変更

--- page_update_list.sh
+++ page_update_list.sh
@@ -1,15 +1,16 @@
 #!/bin/zsh
 #更新を確認したいサイトのurlをconf_listに追加しておくこと
-rm ~/Desktop/todays_update_site.tex
+rm ~/Desktop/todays_update_site.txt
 for url in $(cat conf_list); do
   w3m -dump "$url" > tmp.txt
+  name=$(echo $url|md5)
 #初確認時の処理
-  if ! find $url.txt; then
+  if ! find ./before_page/$name.txt; then
     #echo $url
-    tmp.tex > $url.tex
+    cat tmp.txt > ./before_page/$name.txt
     continue;
   fi
-  if diff $url.tex tmp.tex; then
-    echo $url >> ~/Desktop/todays_update_site.tex
+  if ! diff -q ./before_page/$name.txt tmp.txt; then
+    echo $url >> ~/Desktop/todays_update_site.txt
   fi
 done 
