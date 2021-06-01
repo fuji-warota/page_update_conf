7d620632946e99eadaabfbf6541ec05e7cd6e17b
Fuji_warota <255116p@gs.kochi-tech.ac.jp>
2021-06-01 11:54:06 +0900

    サイトの更新がない場合その旨を追記するように変更

--- page_update_list.sh
+++ page_update_list.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 #更新を確認したいサイトのurlをconf_listに追加しておくこと
-rm ~/Desktop/todays_update_site.txt
+FILE=~/Desktop/todays_update_site.txt
+rm $FILE
+touch $FILE
 for url in $(cat conf_list); do
   w3m -dump "$url" > tmp.txt
   name=$(echo $url|md5)
@@ -11,7 +13,11 @@ for url in $(cat conf_list); do
     continue;
   fi
   if ! diff -q ./before_page/$name.txt tmp.txt; then
-    echo $url >> ~/Desktop/todays_update_site.txt
+    echo $url >> $FILE
     cat tmp.txt > ./before_page/$name.txt
   fi
-done 
+done
+#更新サイトがない場合その旨を追記
+if [ ! -s $FILE ]; then
+  echo "更新はありません" >> $FILE
+fi
